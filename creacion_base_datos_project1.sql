851bc907a8e07cd4db917541ca9974a1767b84f5
AnaGonBu <anagbueno@gmail.com>
2022-11-09 10:41:50 +0100

    creacion_esquema_y base de datos sql

---
+++ creacion_base_datos_project1.sql
@@ -0,0 +1,24 @@
+CREATE SCHEMA IF NOT EXISTS Project_1;
+USE project_1;
+
+CREATE TABLE IF NOT EXISTS data_sql (
+index_sql INT NOT NULL PRIMARY KEY , 
+q10_part_1 VARCHAR (55), 
+q10_part_2 VARCHAR (55), 
+q10_part_3 VARCHAR (55), 
+q10_part_4 VARCHAR (55), 
+q10_part_5 VARCHAR (55), 
+q10_part_6 VARCHAR (55), 
+q10_part_7 VARCHAR (55), 
+q10_part_8 VARCHAR (55), 
+q10_part_9 VARCHAR (55), 
+q10_part_10 VARCHAR (55), 
+q10_part_11 VARCHAR (55), 
+q10_part_12 VARCHAR (55), 
+q10_part_13 VARCHAR (55), 
+q10_part_14 VARCHAR (55), 
+q10_part_15 VARCHAR (55),
+q10_part_16 VARCHAR (55), 
+q10_other VARCHAR (55), 
+d482xta VARCHAR (55)
+);
